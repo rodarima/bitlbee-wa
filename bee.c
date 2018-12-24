c434852f58d81e8643384024f1efaa1bd5e2de5f
Rodrigo <rodrigo@hal>
2018-12-24 19:30:24 +0100

    Add sending messages too

--- bee.c
+++ bee.c
@@ -3,6 +3,8 @@
 #include <bitlbee.h>
 #include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 typedef struct im_connection im_connection_t;
 
@@ -10,23 +12,56 @@ typedef struct {
 	wa_t *wa;
 	im_connection_t *ic;
 	pthread_t thread;
+	int pipe[2];
+	GAsyncQueue *q;
 } wabee_t;
+
+enum event {
+	EVENT_CONNECTED,
+	EVENT_PRIV_MSG,
+	EVENT_UPDATE_USER,
+};
+
+typedef struct {
+	enum event type;
+	void *data;
+} event_t;
+
+static int
+event_add(wabee_t *wb, enum event type, void *data);
+
 
 static int
 cb_priv_msg(void *ptr, priv_msg_t *pm)
 {
-	wabee_t *wb = (wabee_t *) ptr;
-	assert(pm->from);
-	assert(pm->from->jid);
-	assert(pm->text);
-	imcb_buddy_msg(wb->ic, pm->from->jid, pm->text, 0, 0);
-	return 0;
+	return event_add(ptr, EVENT_PRIV_MSG, pm);
 }
 
 static int
 cb_update_user(void *ptr, user_t *u)
 {
-	wabee_t *wb = (wabee_t *) ptr;
+	return event_add(ptr, EVENT_UPDATE_USER, u);
+}
+
+static int
+handle_priv_msg(wabee_t *wb, priv_msg_t *pm)
+{
+	assert(pm->from);
+	assert(pm->from->jid);
+	assert(pm->text);
+
+	imcb_buddy_msg(wb->ic, pm->from->jid, pm->text, 0, 0);
+
+	g_free(pm);
+
+	return 0;
+}
+
+static int
+handle_update_user(wabee_t *wb, user_t *u)
+{
+	/* FIXME: Don't free the user u! */
+
 	assert(u);
 
 	fprintf(stderr, "Updating user %s (%s)\n", u->name, u->jid);
@@ -40,11 +75,71 @@ cb_update_user(void *ptr, user_t *u)
 		imcb_add_buddy(wb->ic, u->jid, NULL);
 	}
 
-	imcb_rename_buddy(wb->ic, u->jid, u->name);
-	imcb_buddy_nick_hint(wb->ic, u->jid, u->notify);
+	if(u->name)
+		imcb_rename_buddy(wb->ic, u->jid, u->name);
+	if(u->notify)
+		imcb_buddy_nick_hint(wb->ic, u->jid, u->notify);
+
 	imcb_buddy_status(wb->ic, u->jid, OPT_LOGGED_IN | OPT_AWAY, NULL,
 		NULL);
 	return 0;
+}
+
+static int
+event_add(wabee_t *wb, enum event type, void *data)
+{
+	event_t *ev;
+
+	ev = g_new0(event_t, 1);
+
+	ev->type = type;
+	ev->data = data;
+
+	g_async_queue_push(wb->q, ev); /* No return errors? */
+
+
+	return 0;
+}
+
+static int
+wabee_send_priv_msg(struct im_connection *ic, char *who, char *text,
+	int __attribute__((unused)) away)
+{
+	wabee_t *wb = ic->proto_data;
+	return wa_send_priv_msg(wb->wa, who, text);
+}
+
+static gboolean
+queue_dispatch(gpointer ptr, gint __attribute__((unused)) fd,
+		     b_input_condition __attribute__((unused)) cond)
+{
+	wabee_t *wb = ptr;
+	event_t *ev;
+
+	//fprintf(stderr, "Dispatching events\n");
+
+	while((ev = g_async_queue_try_pop(wb->q)))
+	{
+
+		switch(ev->type)
+		{
+			case EVENT_PRIV_MSG:
+				handle_priv_msg(wb, ev->data);
+				break;
+			case EVENT_UPDATE_USER:
+				handle_update_user(wb, ev->data);
+				break;
+			case EVENT_CONNECTED:
+				imcb_connected(wb->ic);
+				break;
+			default:
+				fprintf(stderr, "Unknown event type %d\n", ev->type);
+		}
+
+		g_free(ev);
+	}
+
+	return 1;
 }
 
 static wabee_t *
@@ -60,6 +155,12 @@ wabee_new(account_t *a)
 	wb->wa = wa_init(cb);
 	wb->ic = imcb_new(a);
 
+	wb->ic->proto_data = wb;
+
+	wb->q = g_async_queue_new();
+
+	b_timeout_add(50, queue_dispatch, wb);
+
 	return wb;
 }
 
@@ -68,11 +169,15 @@ wabee_loop(void *ptr)
 {
 	wabee_t *wb = (wabee_t *) ptr;
 	wa_login(wb->wa);
-	imcb_connected(wb->ic);
+
+	event_add(wb, EVENT_CONNECTED, NULL);
+
+	/*imcb_connected(wb->ic);*/
 	wa_loop(wb->wa);
 
 	return NULL;
 }
+
 
 static int
 wabee_run(wabee_t *wb)
@@ -120,7 +225,7 @@ G_MODULE_EXPORT void init_plugin(void)
 	ret->options = PRPL_OPT_NOOTR | PRPL_OPT_NO_PASSWORD;
 	ret->name = "whatsapp";
 	ret->login = wabee_login;
-//	ret->buddy_msg = wabee_buddy_msg;
+	ret->buddy_msg = wabee_send_priv_msg;
 //	ret->get_info = wa_get_info;
 //	ret->add_buddy = wa_add_buddy;
 //	ret->remove_buddy = wa_remove_buddy;
